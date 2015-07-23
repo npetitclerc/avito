314fb6e202cc0077d97bd4ca3066ae1cbf95a9a6
Ubuntu <ubuntu@ip-172-31-0-76.ec2.internal>
2015-07-23 10:04:00 +0000

    add make_usercount code

--- make_usercounts_table.py
+++ make_usercounts_table.py
@@ -10,21 +10,21 @@ from sklearn.linear_model import SGDClassifier, Perceptron, PassiveAggressiveCla
 from sklearn.metrics import log_loss
 import time
 
-chunk_size = 100000
+chunk_size = 500000
 engine = create_engine('sqlite:////home/ubuntu/data/avito/db/database.sqlite')
 engine2 = create_engine('sqlite:////home/ubuntu/data/avito/db/database2.sqlite')
 
 for irun, chunk in enumerate(pd.read_sql_query("SELECT UserID FROM UserInfo;", engine, chunksize=chunk_size)):
-    sids = chunk['SearchID']
-    search_temp = pd.read_sql_query("SELECT SearchID, count(*) as c_search FROM trainSearchStream WHERE SearchID in (" + ",".join(map(str, sids)) + ") GROUP BY SearchID;", engine)
-    chunk = pd.merge(chunk, search_temp, how='left', on=['SearchID'])
+    uids = chunk['UserID']
+    #search_temp = pd.read_sql_query("SELECT UserID, count(SearchID) as c_search FROM trainSearchStream WHERE UserID in (" + ",".join(map(str, uids)) + ") GROUP BY UserID;", engine)
+    #chunk = pd.merge(chunk, search_temp, how='left', on=['UserID'])
 
     #TODO add testStream counts
     
-    searchInfo_temp = pd.read_sql_query("SELECT SearchID, UserID, count(*) as uc_search FROM SearchInfo WHERE SearchID in (" + ",".join(map(str, sids)) + ") GROUP BY UserID;", engine)
-    userids = chunk['UserID']
-    del searchInfo_temp['UserID']
-    chunk = pd.merge(chunk, searchInfo_temp, how='left', on=['SearchID'])
+    searchInfo_temp = pd.read_sql_query("SELECT UserID, count(SearchID) as uc_search FROM SearchInfo WHERE UserID in (" + ",".join(map(str, uids)) + ") GROUP BY UserID;", engine)
+    #userids = chunk['UserID']
+    #del searchInfo_temp['UserID']
+    chunk = pd.merge(chunk, searchInfo_temp, how='left', on=['UserID'])
 
     
     #search_temp = pd.read_sql_query("SELECT SearchID, count(*) as sc_search FROM trainSearchStream GROUP BY SearchID;", engine)
@@ -35,13 +35,13 @@ for irun, chunk in enumerate(pd.read_sql_query("SELECT UserID FROM UserInfo;", e
     #ads_temp = pd.read_sql_query("SELECT AdID FROM AdsInfo;", engine)
     #print ads_temp.keys()
     
-    #visit_temp = pd.read_sql_query("SELECT UserID, count(*) as uc_visit FROM VisitsStream WHERE UserID in (" + ",".join(map(str, userids)) + ") GROUP BY UserID;", engine)
+    visit_temp = pd.read_sql_query("SELECT UserID, count(*) as uc_visit FROM VisitsStream WHERE UserID in (" + ",".join(map(str, uids)) + ") GROUP BY UserID;", engine)
     #print visit_temp.keys()
-    #chunk = pd.merge(chunk, visit_temp, how='left', on=['UserID'])
+    chunk = pd.merge(chunk, visit_temp, how='left', on=['UserID'])
 
-    #phone_temp = pd.read_sql_query("SELECT UserID, count(*) as uc_phone FROM PhoneRequestsStream WHERE UserID in (" + ",".join(map(str, userids)) + ") GROUP BY UserID;", engine)
+    phone_temp = pd.read_sql_query("SELECT UserID, count(*) as uc_phone FROM PhoneRequestsStream WHERE UserID in (" + ",".join(map(str, uids)) + ") GROUP BY UserID;", engine)
     #print phone_temp.keys()
-    #chunk = pd.merge(chunk, phone_temp, how='left', on=['UserID'])
+    chunk = pd.merge(chunk, phone_temp, how='left', on=['UserID'])
 
     #chunk['c_ratio_visit'] = chunk['c_visit'] / chunk['c_search'].astype(float)
     #chunk['c_ratio_phone'] = chunk['c_phone'] / chunk['c_search'].astype(float)    
